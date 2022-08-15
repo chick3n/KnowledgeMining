0ba2c5ad90f387fbabc9c4ed12f523b1a8d2b736
Chick3n <chick3n@gmail.com>
2022-08-15 09:49:40 -0400

    uplaod the whole cache not just the search results

--- app/src/KnowledgeMining.UI/Pages/Documents/Documents.razor.cs
+++ app/src/KnowledgeMining.UI/Pages/Documents/Documents.razor.cs
@@ -86,8 +86,9 @@ namespace KnowledgeMining.UI.Pages.Documents
             try
             {
                 await Mediator.Send(new DeleteDocumentCommand(document.Name));
-                _documents = _documents.Where(x => !x.Name.Equals(document.Name));
-                MemoryCache.Set(Constants.DOCUMENT_FILTER_CACHE, _documents);
+                MemoryCache.TryGetValue<IEnumerable<Document>>(Constants.DOCUMENT_FILTER_CACHE, out var cachedDocuments);
+                cachedDocuments = cachedDocuments.Where(x => !x.Name.Equals(document.Name));
+                MemoryCache.Set(Constants.DOCUMENT_FILTER_CACHE, cachedDocuments);
                 Search(_searchText);
 
                 Snackbar.Add("Document deleted", Severity.Success);
