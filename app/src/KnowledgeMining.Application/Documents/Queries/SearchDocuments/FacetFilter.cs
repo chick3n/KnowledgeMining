f7401d7f686a195e84360e16643c39aef3c8e3b3
Chick3n <chick3n@gmail.com>
2022-09-14 11:04:09 -0400

    equals

--- app/src/KnowledgeMining.Application/Documents/Queries/SearchDocuments/FacetFilter.cs
+++ app/src/KnowledgeMining.Application/Documents/Queries/SearchDocuments/FacetFilter.cs
@@ -11,5 +11,46 @@
         {
             Values = new List<string>();
         }
+
+        public override bool Equals(object? obj)
+        {
+            FacetFilter comparison = null;
+
+            try
+            {
+                comparison = obj as FacetFilter;
+            }
+            catch
+            {
+                return false;
+            }
+
+            if (obj != null && obj.GetType().IsSubclassOf(typeof(FacetFilter)))
+            {
+                var equals = true;
+
+                equals = Count == comparison!.Count;
+
+                if ((Name == null && comparison.Name != null) ||
+                    (Name != null && comparison.Name == null))
+                    equals = false;
+                else if (Name != null && !Name.Equals(comparison.Name))
+                    equals = false;
+
+                if (Values != null && comparison.Values != null)
+                {
+                    var values = string.Join(',', Values);
+                    var compValues = string.Join(',', comparison.Values);
+                    if (!values.Equals(compValues))
+                        equals = false;
+                }
+                else if (Values != comparison.Values)
+                    equals = false;
+
+                return equals;
+            }
+
+            return false;
+        }
     }
 }
